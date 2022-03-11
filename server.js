95c3b8a594cfefa8b4e08c71a06de3879d102ab9
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-03-11 17:06:15 -0600

    creates path to index.html

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 
-
-
 app.get('/', (req, res) => {
-    res.send("hello World")
-})
+    console.log(__dirname)
+
+    res.sendFile(__dirname + '/public/index.html');
+  });
 
 app.listen(3001)
