5e454d1ef74e028ec15c471dc7efbc2f9620017d
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-03-11 17:08:14 -0600

    creates path to notes.html

--- server.js
+++ server.js
@@ -7,4 +7,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
   });
 
+app.get('/notes', (req, res) => {
+    console.log(__dirname)
+
+    res.sendFile(__dirname + '/public/notes.html');
+  });
+
 app.listen(3001)
