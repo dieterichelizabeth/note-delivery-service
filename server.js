a3cc813f27352eca193b0ce3b47a03c44422d345
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-03-11 17:38:10 -0600

    updates index to * per requirement

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    console.log(__dirname)
+app.get('*', (req, res) => {
 
     res.sendFile(__dirname + '/public/index.html');
   });
 
 app.get('/notes', (req, res) => {
-    console.log(__dirname)
 
     res.sendFile(__dirname + '/public/notes.html');
   });
