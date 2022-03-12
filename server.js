098c6b75a46d1ee0175389eec5895bb69a66be5e
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-03-12 11:14:11 -0600

    adds completed post route

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const app = express()
+const fs = require('fs');
+const express = require('express');
+const db = require('./db/notes.json');
+const path = require('path');
 
+const app = express()
+// PORT
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// keep html files static
 app.use(express.static("public"))
 
 // display the notes/html file
@@ -17,19 +23,31 @@ app.get('/notes', (req, res) => {
 
 // get the json file
 app.get('/api/notes', (req, res) => {
-    res.sendFile(__dirname + '/db/notes.json');
+    res.json( db );
 });
 
 // save a new note
-app.post('/api/notes', (req, res) => {
-    // save on the request body
-    console.log(req.body);
-    res.json(req.body);
-    // add to the db.json file (with unique id)
-  
-    // return the new note to the client
+app.post('/api/notes', (req, res) => { 
+    // send new note and db to addNote()
+    const note = addNote(req.body, db);
+    res.json(note);    
 })
 
+function addNote(body, db) {
+    console.log(body);
+    // save on the request body
+    const notes = body;
+  
+    // add to the db.json file (with unique id)
+    db.push(notes);
+    fs.writeFileSync(
+        path.join(__dirname, '/db/notes.json'),
+        JSON.stringify({ notes: db }, null, 2)
+      );
+    // return the new note to the client
+    return notes;
+  }
+  
 // delete a note
 app.delete('/api/delete', (req, res) => {
 
@@ -40,5 +58,6 @@ app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
   });
 
-
-app.listen(3001)
+app.listen(PORT, () => {
+   console.log(`Server is running on PORT: ${PORT}`);
+});
