ac314013d8c97a0759b8182e4777761a8d92ed40
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-03-12 12:05:24 -0600

    cleans up code

--- routes/notes.js
+++ routes/notes.js
@@ -14,23 +14,25 @@ router.get('/notes', (req, res) => {
 
 // save a new note
 router.post('/notes', (req, res) => { 
-    // send new note and db to addNote()
+    // adds unique id
     req.body.id = db.notes.length.toString();
+
+    // send new note and db to addNote()
     const note = addNote(req.body, db);
     res.json(note);    
 })
 
 function addNote(body, db) {
-    console.log(body);
     // save on the request body
     const notes = body;
   
-    // add to the db.json file (with unique id)
+    // add to the db.json file 
     db.notes.push(notes);
     fs.writeFileSync(
         path.join(__dirname, '../db/notes.json'),
         JSON.stringify({ notes: db.notes }, null, 2)
       );
+
     // return the new note to the client
     return notes;
   }
