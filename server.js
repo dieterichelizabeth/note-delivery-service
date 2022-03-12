d0168d37b9a05c92b5e1528e3d6739a5e5de1af2
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-03-11 23:14:09 -0600

    re-orders routes

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
 const express = require('express')
 const app = express()
 
-// return the index.html file
-app.get('*', (req, res) => {
-
-    res.sendFile(__dirname + '/public/index.html');
-  });
-
 // return the notes/html file
 app.get('/notes', (req, res) => {
-
-    res.sendFile(__dirname + '/public/notes.html');
+    res.send('displays notes.html')
+    // res.sendFile(__dirname + '/public/notes.html');
   });
 
 // read the db.json file and retunr all saves notes as JSON
 app.get('/api/notes', (req, res) => {
-
+    res.send('We are returning Json')
 });
 
 // save a new note
 app.post('/api/notes', (req, res) => {
+    res.send('We are adding a new note')
     // save on the request body
-
     // add to the db.json file (with unique id)
-
     // return the new note to the client
 })
 
 // delete a note
 app.delete('/api/delete', (req, res) => {
-    
+    res.send('We are deleting a note')
 })
+
+// return the index.html file
+app.get('*', (req, res) => {
+    res.send('displays index.html')
+    // res.sendFile(__dirname + '/public/index.html');
+  });
+
 
 app.listen(3001)
